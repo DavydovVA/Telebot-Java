08eb4bb7144584a5a7a19fb23194d1321707aabf
DavydovVA <volerej@gmail.com>
2020-04-02 11:53:43 +0300

    remove Test into Application

--- src/main/java/com.davydov/Application.java
+++ src/main/java/com.davydov/Application.java
@@ -1,13 +1,45 @@
 package com.davydov;
 
+import com.davydov.database.Peripheral;
+import com.davydov.telebot.SimpleBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main (String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.setLogStartupInfo(false);
+
+        ApplicationContext context = app.run(args);
+        RunBotApplication bean = context.getBean(RunBotApplication.class);
+        bean.run();
+
+    }
+
+    @Component
+    public class RunBotApplication {
+        @Autowired
+        private Peripheral peripheral;
+
+        public void run() {
+            ApiContextInitializer.init();
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+            try {
+                telegramBotsApi.registerBot(new SimpleBot(peripheral));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
